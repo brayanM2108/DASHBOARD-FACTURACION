dbf410766a80a6671ce144b50ad3893b25ff7456
Brayan Melo <brayan.acevedo28@gmail.com>
2026-02-19 11:12:25 -0500

    Add: import

--- DASHBOARD_FACTU/ui/tabs/__init__.py
+++ DASHBOARD_FACTU/ui/tabs/__init__.py
@@ -5,3 +5,4 @@ Módulo de pestañas del dashboard
 from .tab_legalizaciones import render_tab_legalizacion
 from .tab_rips import render_tab_rips
 from .tab_facturacion import render_tab_facturacion
+from .tab_procesos import render_tab_procesos
